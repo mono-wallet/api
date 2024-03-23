c7710d1b51bc6aee3e0f3e6a7c02163bd19aad83
Max Levchuk <mlevchuk@yukon.cv.ua>
2024-03-23 10:28:17 +0200

    [CHANGED] express to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from "@nestjs/core"
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify"
 import { AppModule } from "./app.module"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
 
   const config = new DocumentBuilder()
     .setTitle("mono-wallet")
