fea7e2c5a12e20b3f9ad0279c12092451cdfd32b
unisol1020 <unisol1020@icloud.com>
2024-08-13 16:30:48 +0300

    [INIT]

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Mono Wallet API')
+    .setVersion('0.1')
+    .addTag('mono-wallet')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
